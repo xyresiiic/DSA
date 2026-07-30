1143bf1941040e86fa51e0ab83b66df7cb2e81d2
Veer Pratap Singh <xyresiiic@gmail.com>
2026-07-30 19:18:47 +0530

    add program

---
+++ linearsearch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+
+    int result = linearSearch(arr, n, key);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
