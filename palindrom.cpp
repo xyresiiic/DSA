8b1e32bccfe3a2e99613ebffc81c0b5b4736d6e2
Veer Pratap Singh <xyresiiic@gmail.com>
2026-08-08 13:42:51 +0530

    update

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,38 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class solution
-{
-    public:
-    bool isPalindrome(string s)
-    {
-            int left = 0;
-            int right = s.length() - 1;
-
-            while (left < right) 
-            {
-                if (s[left] != s[right]) 
-                {
-                    return false;
-                }
-                left++;
-                right--;
-            }
-            return true;
-        }
-
-};
-
 int main()
 {
-    solution sol;
-    string s = "racecar";
-    if (sol.isPalindrome(s)) {
-        cout << "Yes" << endl;
-    } 
-    else 
-    {
-        cout << "No" << endl;
-    }
-    return 0;
+    
 }
